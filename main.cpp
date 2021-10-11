e1fcc841abddc5efef286671c0c0a78ab77fd27d
ilyabenikov <vatnaya1997@gmail.com>
2021-10-11 20:07:22 +0300

    Update cube solve.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int const rSIZE = 3;
 
-void searchByX(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light);
+bool searchByX(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z);
 
-void searchByY(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light);
+bool searchByY(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z);
 
-void searchByZ(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light);
+bool searchByZ(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z);
 
 void fillingCube(int array[rSIZE][rSIZE][rSIZE]);
 
@@ -18,87 +18,61 @@ int getTrueNum(int num);
 int main() {
     srand(time(NULL));
 
-    int particleLight = 0;
     int light = 0;
 
     int array[rSIZE][rSIZE][rSIZE];
 
     fillingCube(array);
-    searchByX(array, particleLight, light);
-    searchByY(array, particleLight, light);
-    searchByZ(array, particleLight, light);
 
-
-}
-
-void searchByX(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light) {
-    for (int x = 0; x < rSIZE; x++) {
-        for (int y = 0; y < rSIZE; y++) {
-            for (int z = 0; z < rSIZE; z++) {
-                if (x == y && y == z && z == x && array[x][y][z] == 0) {
-                    particleLight++;
-                    if (array[getTrueNum(x + 1)][y][z] == 0) {
-                        particleLight++;
-                        if (array[getTrueNum(x + 2)][y][z] == 0) {
-                            particleLight++;
-                            if (particleLight == rSIZE) {
-                                light++;
-                                cout << "Count lights X:" << light << " [x][y][z]" << x << y << z << " "
-                                     << getTrueNum(x + 1) << y << z << " " << getTrueNum(x + 2) << y << z << endl;
-                            }
-                        }
-                    }
+    for (int z = 0; z < rSIZE; ++z) {
+        for (int y = 0; y < rSIZE; ++y) {
+            for (int x = y; x <= y; ++x) {
+                if (searchByX(array, x, y, z)) {
+                    light++;
+                }
+                if (searchByY(array, x, y, z)) {
+                    light++;
+                }
+                if (searchByZ(array, x, y, z)) {
+                    light++;
                 }
             }
         }
     }
+    cout << "Count lights:" << light;
 }
 
-void searchByY(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light) {
-    for (int x = 0; x < rSIZE; x++) {
-        for (int y = 0; y < rSIZE; y++) {
-            for (int z = 0; z < rSIZE; z++) {
-                if (x == y && y == z && z == x && array[x][y][z] == 0) {
-                    particleLight++;
-                    if (array[x][getTrueNum(y + 1)][z] == 0) {
-                        particleLight++;
-                        if (array[x][getTrueNum(y + 2)][z] == 0) {
-                            particleLight++;
-                            if (particleLight == rSIZE) {
-                                light++;
-                                cout << "Count lights Y:" << light << " [x][y][z]" << x << y << z << " " << x
-                                     << getTrueNum(y + 1)
-                                     << z << " " << x << getTrueNum(y + 2) << z << endl;
-                            }
-                        }
-                    }
-                }
+bool searchByX(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z) {
+    if (array[x][y][z] == 0) {
+        if (array[getTrueNum(x + 1)][y][z] == 0) {
+            if (array[getTrueNum(x + 2)][y][z] == 0) {
+                return true;
             }
         }
     }
+    return false;
 }
 
-void searchByZ(int array[rSIZE][rSIZE][rSIZE], int particleLight, int light) {
-    for (int x = 0; x < rSIZE; x++) {
-        for (int y = 0; y < rSIZE; y++) {
-            for (int z = 0; z < rSIZE; z++) {
-                if (x == y && y == z && z == x && array[x][y][z] == 0) {
-                    particleLight++;
-                    if (array[x][y][getTrueNum(z + 1)] == 0) {
-                        particleLight++;
-                        if (array[x][y][getTrueNum(z + 2)] == 0) {
-                            particleLight++;
-                            if (particleLight == rSIZE) {
-                                light++;
-                                cout << "Count lights Z:" << light << " [x][y][z]" << x << y << z << " " << x
-                                     << y << getTrueNum(z + 1) << " " << x << y << getTrueNum(z + 2) << endl;
-                            }
-                        }
-                    }
-                }
+bool searchByY(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z) {
+    if (array[x][y][z] == 0) {
+        if (array[x][getTrueNum(y + 1)][z] == 0) {
+            if (array[x][getTrueNum(y + 2)][z] == 0) {
+                return true;
             }
         }
     }
+    return false;
+}
+
+bool searchByZ(int array[rSIZE][rSIZE][rSIZE], int x, int y, int z) {
+    if (array[x][y][z] == 0) {
+        if (array[x][y][getTrueNum(z + 1)] == 0) {
+            if (array[x][y][getTrueNum(z + 2)] == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 void fillingCube(int array[rSIZE][rSIZE][rSIZE]) {
@@ -106,8 +80,11 @@ void fillingCube(int array[rSIZE][rSIZE][rSIZE]) {
         for (int y = 0; y < rSIZE; y++) {
             for (int z = 0; z < rSIZE; z++) {
                 array[x][y][z] = rand() % 2;
+                cout << array[x][y][z];
             }
+            cout << endl;
         }
+        cout << endl;
     }
 }
 
